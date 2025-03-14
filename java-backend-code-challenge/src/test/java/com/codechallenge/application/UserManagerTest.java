5a441044381e8a6f438cc4fa0dfa6f7fa03c116d
Francisco <fdominguez20000@gmail.com>
2025-03-14 16:34:45 +0000

    Feat: UserFinderTest and UsersFinderTest merge into UserManagerTest

--- java-backend-code-challenge/src/test/java/com/codechallenge/application/UserManagerTest.java
+++ java-backend-code-challenge/src/test/java/com/codechallenge/application/UserManagerTest.java
@@ -1,6 +1,5 @@
 package com.codechallenge.application;
 
-import com.codechallenge.application.ports.driven.UserRepositoryStub;
 import com.codechallenge.application.ports.driven.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,17 +12,63 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class UsersFinderTest {
+public class UserManagerTest {
 
     @Mock
     private UserRepository userRepositoryStub;
+    private UserFinder userFinder = new UserFinder(userRepositoryStub);
+    private UsersFinder usersFinder = new UsersFinder(userRepositoryStub);
+
+    @Test
+    public void doesNotFoundUser() {
+        //Arrange
+        userFinder = new UserFinder(userRepositoryStub);
+        Mockito.when(userRepositoryStub.getById("leUser")).thenReturn(null);
+
+        //Act
+        var user = userFinder.execute("leUser");
+
+        //assert
+        assertEquals(null, user);
+    }
+
+    @Test
+    public void doesFoundUser() {
+        //Arrange
+        userFinder = new UserFinder(userRepositoryStub);
+        User userInStub = new User("TestUsername", "TestName", "TestEmail", "TestGender", "TestPicture");
+        Mockito.when(userRepositoryStub.getById("TestUsername")).thenReturn(userInStub);
+
+        //Act
+        User user = userFinder.execute("TestUsername");
+
+        //assert
+        assertEquals(userInStub,user);
+    }
+
+    @Test
+    public void doesFoundUserValuesMatch() {
+        //Arrange
+        userFinder = new UserFinder(userRepositoryStub);
+        User userInStub = new User("TestUsername", "TestName", "TestEmail", "TestGender", "TestPicture");
+        Mockito.when(userRepositoryStub.getById("TestUsername")).thenReturn(userInStub);
+        //Act
+        var user = userFinder.execute("TestUsername");
+
+        //assert
+        assertEquals("TestUsername", user.username(), "username value");
+        assertEquals("TestName", user.name(), "name value");
+        assertEquals("TestEmail", user.email(), "email value");
+        assertEquals("TestGender", user.gender(), "gender value");
+        assertEquals("TestPicture", user.picture(), "picture value");
+    }
+
 
     @Test
     public void doesNotContainUsers() {
         //Arrange
+        usersFinder = new UsersFinder(userRepositoryStub);
         Mockito.when(userRepositoryStub.getAll()).thenReturn(List.of());
-
-        UsersFinder usersFinder = new UsersFinder(userRepositoryStub);
 
         //Act
         var users = usersFinder.execute();
@@ -35,12 +80,12 @@ public class UsersFinderTest {
     @Test
     public void doesContainUsers() {
         //Arrange
+        usersFinder = new UsersFinder(userRepositoryStub);
         Mockito.when(userRepositoryStub.getAll()).thenReturn(List.of(
                 new User("username", "name", "email", "gender", "picture"),
                 new User("UserB", "UserB", "UserB", "UserB", "UserB"),
                 new User("UserC", "UserC", "UserC", "UserC", "UserC"),
                 new User("UserD", "UserD", "UserD", "UserD", "UserD")));
-        UsersFinder usersFinder = new UsersFinder(userRepositoryStub);
 
         //Act
         var users = usersFinder.execute();
@@ -53,7 +98,7 @@ public class UsersFinderTest {
     public void doesUsersMatch() {
         //Arrange
 
-        UsersFinder usersFinder = new UsersFinder(userRepositoryStub);
+        usersFinder = new UsersFinder(userRepositoryStub);
         List<User> userInStub = List.of(
                 new User("username", "name", "email", "gender", "picture"),
                 new User("UserB", "UserB", "UserB", "UserB", "UserB"),
@@ -68,8 +113,4 @@ public class UsersFinderTest {
         assertEquals(userInStub.get(2), users.get(2));
         assertEquals(userInStub.get(3), users.get(3));
     }
-
-
 }
-
-
