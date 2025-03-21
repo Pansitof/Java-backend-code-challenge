d4da0b2bcf8e9fc0e4383206e1d3abae34efa42b
Francisco <fdominguez20000@gmail.com>
2025-03-21 15:29:26 +0000

    Feat: new test for User Put. Able to update only some values on an User

--- java-backend-code-challenge/src/main/java/com/codechallenge/application/usecase/UserPut.java
+++ java-backend-code-challenge/src/main/java/com/codechallenge/application/usecase/UserPut.java
@@ -2,33 +2,52 @@ package com.codechallenge.application.usecase;
 
 import com.codechallenge.application.domain.User;
 import com.codechallenge.application.ports.driven.UserRepository;
+import com.codechallenge.application.usecase.exception.EmailAlreadyInUseException;
 import com.codechallenge.application.usecase.exception.EmailInvalidFormatException;
 import com.codechallenge.application.usecase.exception.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class UserPut {
     private UserRepository repository;
 
-    public UserPut(UserRepository repository){
+    public UserPut(UserRepository repository) {
         this.repository = repository;
     }
 
     public void execute(String username, String name, String email, String gender) {
         Optional<User> searchedUser = repository.getById(username);
-        if (searchedUser.isEmpty()){
+        if (searchedUser.isEmpty()) {
             throw new UserNotFoundException("There isn't an user with that ID");
         }
+        if (repository.getByEmail(email).isPresent()) {
+            throw new EmailAlreadyInUseException();
+        }
         Pattern allowedEmailPattern = Pattern.compile("^(.+)@(.+)\\.(.+)$");
-        if(!allowedEmailPattern.matcher(email).find()) {
+        if (!allowedEmailPattern.matcher(email).find() && !email.isEmpty()) {
             throw new EmailInvalidFormatException("Email has Incorrect Format");
         }
-        User newUser = new User(username,name,email,gender,searchedUser.get().picture());
+        String newName = searchedUser.get().name();
+        String newEmail = searchedUser.get().email();
+        String newGender = searchedUser.get().gender();
+        if (!name.isEmpty()) {
+            newName = name;
+        }
+        if (!email.isEmpty()) {
+            newEmail = email;
+        }
+        if (!gender.isEmpty()) {
+            newGender = gender;
+        }
+
+        User newUser = new User(username, newName, newEmail, newGender, searchedUser.get().picture());
+
 
         repository.modifyUser(newUser);
     }
-
 
 
 }
--- java-backend-code-challenge/src/test/java/com/codechallenge/application/usecase/UserPutShould.java
+++ java-backend-code-challenge/src/test/java/com/codechallenge/application/usecase/UserPutShould.java
@@ -2,6 +2,7 @@ package com.codechallenge.application.usecase;
 
 import com.codechallenge.application.domain.User;
 import com.codechallenge.application.ports.driven.UserRepository;
+import com.codechallenge.application.usecase.exception.EmailAlreadyInUseException;
 import com.codechallenge.application.usecase.exception.EmailInvalidFormatException;
 import com.codechallenge.application.usecase.exception.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
@@ -57,6 +58,28 @@ public class UserPutShould {
     }
 
     @Test
+    public void onlyUpdateSomeValues(){
+        String username = "testUsername";
+        String newGender = "newGender";
+        User oldUser = UserMother.createUser(username, "testName", "testEmail@email.es", "testGender", "testPicture");
+        Mockito.when(userRepository.getById(username)).thenReturn(Optional.of(oldUser));
+
+        userPut.execute(username, "", "", newGender);
+
+        Mockito.verify(userRepository).modifyUser(userCaptor.capture());
+        User userCaptured = userCaptor.getValue();
+
+        assertAll(
+                "Grouped Assertions of Expected modified User data",
+                () -> assertEquals(username, userCaptured.username()),
+                () -> assertEquals("testName", userCaptured.name()),
+                () -> assertEquals("testEmail@email.es", userCaptured.email()),
+                () -> assertEquals(newGender, userCaptured.gender()),
+                () -> assertEquals("testPicture", userCaptured.picture())
+        );
+    }
+
+    @Test
     public void failByUserNotExisting() {
         String usernameTest = "TestUsername";
         Mockito.when(userRepository.getById(usernameTest)).thenReturn(Optional.empty());
@@ -84,6 +107,16 @@ public class UserPutShould {
 
     @Test
     public void failByEmailAlreadyUsed() {
+        String username = "testUsername";
+        String email = "testEmail@email.es";
+        User oldUser = UserMother.createUser(username, "testName", email, "testGender", "testPicture");
+        Mockito.when(userRepository.getByEmail(email)).thenReturn(Optional.of(oldUser));
+        Mockito.when(userRepository.getById(username)).thenReturn(Optional.of(oldUser));
 
+        Exception exception = assertThrows(EmailAlreadyInUseException.class, () -> {
+            userPut.execute(username,"", email,"");
+        });
+
+        assertEquals("That email is already in use by another account", exception.getMessage());
     }
 }
