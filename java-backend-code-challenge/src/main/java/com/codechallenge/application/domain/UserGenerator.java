8abbb3dfc925df31cee4ac9309106298705c7dd9
Francisco <fdominguez20000@gmail.com>
2025-03-24 14:12:41 +0000

    Feat: Functional UserGenerator with Test

--- java-backend-code-challenge/src/main/java/com/codechallenge/application/domain/UserGenerator.java
+++ java-backend-code-challenge/src/main/java/com/codechallenge/application/domain/UserGenerator.java
@@ -1,8 +1,30 @@
 package com.codechallenge.application.domain;
 
-public class UserGenerator {
-    public User generateAnUser(){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
 
-        return new User("","","","","");
+public class UserGenerator {
+    public User generateAnUser() throws IOException {
+        URL url = new URL("https://randomuser.me/api/?inc=name,email,gender");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String usuario = br.readLine();
+        br.close();
+
+        String[] array = usuario.split("\"\\W*\"");
+
+        String[] array2 = Arrays.toString(array).split("\\W+\\s*");
+
+        String UserName = array2[8] + array2[10];
+        String Name = array2[8];
+        String Email = UserName + "@example.com";
+        String Genre = array2[3];
+        String Picture = Name+"_"+new NumberGenerator().generateFourRandomsDigits();
+        return new User(UserName,Name,Email,Genre,Picture);
     }
 }
--- java-backend-code-challenge/src/test/java/com/codechallenge/application/domain/UserGeneratorShould.java
+++ java-backend-code-challenge/src/test/java/com/codechallenge/application/domain/UserGeneratorShould.java
@@ -2,15 +2,30 @@ package com.codechallenge.application.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserGeneratorShould {
     private UserGenerator userGenerator;
 
-    public UserGeneratorShould(UserGenerator userGenerator) {
-        this.userGenerator = userGenerator;
-    }
-
     @Test
-    public void generateRandomUser(){
-
+    public void generateRandomUser() {
+        userGenerator = new UserGenerator();
+        User user;
+        try {
+            user = userGenerator.generateAnUser();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertAll(
+                "Grouped Assertions of Generated User",
+                () -> assertFalse(user.username().isBlank()),
+                () -> assertFalse(user.name().isBlank()),
+                () -> assertFalse(user.email().isBlank()),
+                () -> assertFalse(user.gender().isBlank()),
+                () -> assertFalse(user.picture().isBlank())
+        );
     }
 }
